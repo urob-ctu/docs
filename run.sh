8d1139eeb1da95c8631b8a799109f039a22a5059
Jan Vlk <vlkjan6@fel.cvut.cz>
2024-09-02 15:43:32 +0200

    Update docker with revision requests

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/bash
-bundle exec jekyll serve -l -o
+if ! bundle exec jekyll serve -l -o
+then
+    docker run --network="host" -v ./docs:/usr/src/docs/docs -v ./assets:/usr/src/docs/assets -t docs
+fi
--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-docker run --network="host" -t docs

a1b5e8c64f1e3c77228dcaff67d9693601c502ef
Jan Vlk <vlkjan6@fel.cvut.cz>
2024-09-02 20:30:19 +0200

    Fix build.sh flags processing

--- build.sh
+++ build.sh
@@ -10,5 +10,20 @@ Help()
     echo "h     Print this help message."
 }
 
+# Deal with flags
+force_docker=false
+while getopts ":h" option; do
+    case $option in
+        h)  # display Help
+            Help
+            exit;;
+        \?)  # invalid option
+            echo "Error: Invalid option"
+            echo
+            Help
+            exit;;
+    esac
+done
+
 # Main
 docker build -t docs .

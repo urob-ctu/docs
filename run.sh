56a54510a55c67c4c954140ee770b7472eae2bb0
Jan Vlk <vlkjan6@fel.cvut.cz>
2024-09-02 16:11:42 +0200

    If docker image not build automatically build it in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
-if ! bundle exec jekyll serve -l -o
-then
+if ! bundle exec jekyll serve -l -o; then
+    if [[ $(docker image inspect docs 2>/dev/null) == "[]" ]]; then
+        echo "Building docker image"
+        bash ./build.sh
+    fi
+    echo "Running in docker"
     docker run --network="host" -v ./docs:/usr/src/docs/docs -v ./assets:/usr/src/docs/assets -t docs
 fi
